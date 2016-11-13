18fcd57dd40b065e9b26b899eb34449f9fd967f2
pongsanti <pongsanti.tanvejsilp@gmail.com>
2016-11-13 20:30:07 +0700

    start button on the first page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  get 'welcome/home'
   get 'welcome/index'
 
-  root 'welcome#index'
+  root 'welcome#home'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
